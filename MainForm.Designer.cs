859ef826310d7a715040a6d0d2c2c1893c86eb11
Jay Cuadro <jaycuadro@gmail.com>
2013-05-05 18:09:45 +0800

    Initial version
    Initial version

---
+++ MainForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace LoLConnectionTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnPing = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.btnKill = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnRestartModem = new System.Windows.Forms.Button();
+            this.webModem = new System.Windows.Forms.WebBrowser();
+            this.timerCountdown = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnPing
+            // 
+            this.btnPing.Location = new System.Drawing.Point(12, 12);
+            this.btnPing.Name = "btnPing";
+            this.btnPing.Size = new System.Drawing.Size(75, 23);
+            this.btnPing.TabIndex = 0;
+            this.btnPing.Text = "Ping LoL";
+            this.btnPing.UseVisualStyleBackColor = true;
+            this.btnPing.Click += new System.EventHandler(this.btnPing_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(12, 88);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtStatus.Size = new System.Drawing.Size(194, 160);
+            this.txtStatus.TabIndex = 1;
+            // 
+            // btnKill
+            // 
+            this.btnKill.Location = new System.Drawing.Point(93, 12);
+            this.btnKill.Name = "btnKill";
+            this.btnKill.Size = new System.Drawing.Size(113, 23);
+            this.btnKill.TabIndex = 2;
+            this.btnKill.Text = "Kill LoL Processes";
+            this.btnKill.UseVisualStyleBackColor = true;
+            this.btnKill.Click += new System.EventHandler(this.btnKill_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 41);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnRestartModem
+            // 
+            this.btnRestartModem.Location = new System.Drawing.Point(93, 41);
+            this.btnRestartModem.Name = "btnRestartModem";
+            this.btnRestartModem.Size = new System.Drawing.Size(113, 23);
+            this.btnRestartModem.TabIndex = 4;
+            this.btnRestartModem.Text = "Restart Modem";
+            this.btnRestartModem.UseVisualStyleBackColor = true;
+            this.btnRestartModem.Click += new System.EventHandler(this.btnRestartModem_Click);
+            // 
+            // webModem
+            // 
+            this.webModem.Location = new System.Drawing.Point(353, 12);
+            this.webModem.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webModem.Name = "webModem";
+            this.webModem.Size = new System.Drawing.Size(128, 236);
+            this.webModem.TabIndex = 5;
+            this.webModem.Visible = false;
+            this.webModem.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webModem_DocumentCompleted);
+            // 
+            // timerCountdown
+            // 
+            this.timerCountdown.Interval = 30000;
+            this.timerCountdown.Tick += new System.EventHandler(this.timerCountdown_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 265);
+            this.Controls.Add(this.webModem);
+            this.Controls.Add(this.btnRestartModem);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnKill);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnPing);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoL Connection Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPing;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Button btnKill;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnRestartModem;
+        private System.Windows.Forms.WebBrowser webModem;
+        private System.Windows.Forms.Timer timerCountdown;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+
+namespace LoLConnectionTool
+{
+    public partial class MainForm : Form
+    {
+        #region variables
+        const string REBOOT_URL = "http://192.168.254.1/reboot.asp";
+        #endregion
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnPing_Click(object sender, EventArgs e)
+        {
+            Ping pinger = new Ping();
+            PingOptions pingOptions = new PingOptions();
+
+            byte[] buffer = new byte[32];
+
+            pingOptions.DontFragment = true;
+            PingReply pingReply = pinger.Send("125.5.6.1");
+
+            if (pingReply.Status == IPStatus.Success)
+            {
+                txtStatus.Text += Environment.NewLine +
+                    string.Format("Reply from {0}: bytes={1} time={2}ms TTL={3}", pingReply.Address, pingReply.Buffer.Length, pingReply.RoundtripTime, pingReply.Options.Ttl);
+            }
+            else
+            {
+                txtStatus.Text += Environment.NewLine + "Destination host unreachable!";
+            }
+        }
+
+        private void btnKill_Click(object sender, EventArgs e)
+        {
+            
+            var lolClient = from Process p in Process.GetProcesses() where p.ProcessName.ToLower() == "lolclient" select p;
+            var lolExe = from Process p in Process.GetProcesses() where p.ProcessName.ToLower() == "league of legends" select p;
+            
+            if ((lolClient.Count() > 0 || lolExe.Count() > 0) &&
+                (MessageBox.Show("Are you sure you want to terminate LoL processes?",
+                    "CONFIRM LOL TERMINATE",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.Yes))
+            {
+                if (lolClient.Count() > 0)
+                {
+                    lolClient.First().Kill();
+                    txtStatus.Text += Environment.NewLine +
+                        "Sucessfully killed " + lolClient.First().ProcessName;
+                }
+                else
+                {
+                    txtStatus.Text += Environment.NewLine +
+                    "Cannot find LoLClient in running processes.";
+                }
+                
+                System.Threading.Thread.Sleep(1000);
+
+                if (lolExe.Count() > 0)
+                {
+                    lolExe.First().Kill();
+                    txtStatus.Text += Environment.NewLine +
+                        "Sucessfully killed " + lolExe.First().ProcessName;
+                }
+                else
+                {
+                    txtStatus.Text += Environment.NewLine +
+                        "Cannot find League of Legends in running processes.";
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtStatus.Text = string.Empty;
+        }
+
+        private void webModem_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+            if (webModem.Url.ToString() == REBOOT_URL)
+            {
+                HtmlDocument htmlDoc = webModem.Document;
+                HtmlElementCollection buttons = htmlDoc.GetElementsByTagName("input");
+                var resetButton = from HtmlElement element in htmlDoc.GetElementsByTagName("input")
+                                  where element.Name == "reboot"
+                                  select element;
+
+                if (resetButton.Count() > 0)
+                {
+                    resetButton.First().InvokeMember("click");
+                    timerCountdown.Enabled = true;
+                    btnRestartModem.Enabled = false;
+                    btnRestartModem.Text = "Restarting....";
+                    webModem.AllowNavigation = false;
+                }
+                else
+                {
+                    MessageBox.Show("Cannot find the  reset button.",
+                        "FAIL TO RESTART MODEM",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                webModem.AllowNavigation = false;
+            }
+        }
+
+        private void btnRestartModem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to restart your modem?",
+                "CONFIRM RESTART",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.Yes)
+            {
+                webModem.AllowNavigation = true;
+                webModem.Navigate(REBOOT_URL);
+            }
+            
+            
+        }
+
+        private void timerCountdown_Tick(object sender, EventArgs e)
+        {
+            btnRestartModem.Enabled = true;
+            btnRestartModem.Text = "Restart Modem";
+            timerCountdown.Enabled = false;
+        }
+    }
+}
